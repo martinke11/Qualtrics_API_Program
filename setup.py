5e7123e29fa10f3421cdb4be93236b5e4fef216c
Kieran Martin <128542036+martinke11@users.noreply.github.com>
2026-07-22 16:46:50 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 10:25:08 2025
+
+@author: 484843
+"""
+from setuptools import setup
+
+# read your requirements.txt, if you like
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="qualtrics_api_program",
+    version="0.1.0",
+    py_modules=["config", "QualAPI"],
+    install_requires=requirements,
+)
+
