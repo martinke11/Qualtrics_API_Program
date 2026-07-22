c4b2999586c64575198d9174bc59fc6ccc4e0f84
Kieran Martin <128542036+martinke11@users.noreply.github.com>
2026-07-22 16:33:00 -0500

    Update nlp_analysis.py

--- nlp_analysis.py
+++ nlp_analysis.py
@@ -19,7 +19,6 @@ import requests
 from googletrans import Translator
 import json
 import os
-os.chdir('/Users/kieranmartin/Documents/Qualtrics_API_Program')
 import time
 #import emoji
 import warnings
@@ -31,13 +30,22 @@ warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 warnings.simplefilter(action='ignore', category=pd.errors.DtypeWarning)
 pd.options.mode.chained_assignment = None 
 
-
 # Ensure that all output is printed without truncation
 pd.set_option('display.max_colwidth', None)  # Do not truncate column content
 pd.set_option('display.max_rows', None)  # Show all rows
 pd.set_option('display.max_columns', None)  # Show all columns
 
-with open('/Users/kieranmartin/Documents/Qualtrics_API_Program/qualtrics_credentials.txt') as f:
-    creds = json.load(f)
+from config import (
+    set_project_directory,
+    get_qualtrics_credentials_path
+)
+
+PROJECT_DIRECTORY = set_project_directory()
+print("Working directory changed to:", PROJECT_DIRECTORY)
+
+QUALTRICS_CREDENTIALS_PATH = get_qualtrics_credentials_path()
+print("Qualtrics credentials path:", QUALTRICS_CREDENTIALS_PATH)
+with open(QUALTRICS_CREDENTIALS_PATH) as f:
+    qualtrics_creds = json.load(f)
     
 # build out from here 
